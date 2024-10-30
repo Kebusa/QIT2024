1023afbe2b6f46cb750ed6d2c880488608754c52
Asubek <asubekerakyluly@gmail.com>
2024-10-30 14:43:43 +0500

    home work 30.10

---
+++ QIT2024/homeworks/30.10/script.js
@@ -0,0 +1,14 @@
+
+let model = "Daewoo matis "
+let color = "green"
+let wheels = 4
+let isOffRoad = false
+let rating = [3, 5]
+
+
+console.log("modelName:", model);
+console.log("color:", color);
+console.log("WheelCount:", wheels);
+console.log("isOffRoad:", isOffRoad);
+console.log("ratting", rating);
+
