ef3a8ce6699d49310d6bb2884a379199d9f140e3
Asubek <asubekerakyluly@gmail.com>
2024-10-30 12:16:18 +0500

    oz

--- QIT2024/Examples/30.10/user.js
+++ QIT2024/Examples/30.10/user.js
@@ -1,6 +1,14 @@
 let season = "fall";
-console.log(season === "summer");
+console.log(season === "summer"); //boolean
 
 let name = "Asubek" //string
 let age = "19" //number
 let grades = [3, 4, 5, 9] //Array
+
+let isStudent = true //boolean
+let hasLicense = false //boolean
+
+console.log("onyn aty", name);
+console.log("jasy", age);
+console.log("onyn bagasy", grades);
+
