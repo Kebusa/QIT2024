ef304a4fcdae1b0de96ed90dfe4819896b05c3bd
Asubek <asubekerakyluly@gmail.com>
2024-10-30 12:32:23 +0500

    it

--- QIT2024/Examples/30.10/user.js
+++ QIT2024/Examples/30.10/user.js
@@ -3,12 +3,26 @@ console.log(season === "summer"); //boolean
 
 let name = "Asubek" //string
 let age = "19" //number
-let grades = [3, 4, 5, 9] //Array
+let surname = "Erakyl";
+let scool = "24 lyceum";
+let birth = [12, 4, 2005];
+
 
 let isStudent = true //boolean
 let hasLicense = false //boolean
 
+let car = {
+    name: "Nissan",
+    mark: "skyline",
+    color: "aquablue",
+    number: "777 ASU",
+    year: 2001
+} //object
+
 console.log("onyn aty:", name);
 console.log("jasy:", age);
-console.log("onyn bagasy:", grades);
+console.log("familia:", surname);
+console.log("shkola:", scool);
+console.log("tugan kun:", birth);
+console.log( car)
 
