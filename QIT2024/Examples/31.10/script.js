0b733b78b2603241a9e83808b3793908410b0c94
Asubek <asubekerakyluly@gmail.com>
2024-10-31 12:02:11 +0500

    zadanie

---
+++ QIT2024/Examples/31.10/script.js
@@ -0,0 +1,66 @@
+// let x = 20 
+// let y = 30
+// let result = x - y
+
+// console.log(result)
+
+// let x = 10 
+// let y = 5 
+// let result = x + y
+// console.log(result)
+
+// let x = 10 
+// let y = 5 
+// let result = x * y 
+ 
+// console.log(result)
+
+// let x = 50
+// let y = 5 
+// let result = x / y 
+
+// console.log(result)
+
+
+// var x = 20
+
+// if(x % 2 ==0){
+//     console.log(x, "x jup san ")
+
+// }
+
+
+// var x = 200
+// x++ //increment
+// x-- //decrement
+// console.log(x)
+
+
+// var x = 100
+// x += 10 //x = x + 10
+// x -= 10 //x = x - 10
+// x *= 10 //x = x * 10
+// x /= 10 // x = x / 10
+// x %= 10 // x = x % 10
+// console.log(x)
+
+
+// let a = 12
+// let b = 20
+// let c = 5 
+// let d = 7 
+// let e = 3
+
+// let total = (a + b) * c - d / e
+
+// console.log(total % 2)
+
+
+let a = 10
+let b = 5 
+let c = 5
+let z = 7
+
+let res = ((a * b) / c) / (z - 3)
+ 
+console.log(res)
