36fc30635d16125f055325066cad2fd82e6e6737
Asubek <asubekerakyluly@gmail.com>
2024-11-03 21:09:22 +0500

    31.10 home work

---
+++ QIT2024/homeworks/31.10/script.js
@@ -0,0 +1,21 @@
+// task1 Pizza
+var slicesPerPiza = 8
+var numberOfPizza = 5
+
+totalSlices = slicesPerPiza * numberOfPizza
+
+console.log("TotalSlices:", totalSlices)
+
+// task2 Chocolate
+var totalchocolates = 24
+var numberOfFriends = 6
+
+ChocolatesPerFriend = totalchocolates / numberOfFriends
+
+console.log("ChocolatesPerFriend:", ChocolatesPerFriend)
+
+// task3 Plant
+var plantHeight = 10
+plantHeight +=2
+
+console.log("PlantHeghtOnWeek:", plantHeight)
