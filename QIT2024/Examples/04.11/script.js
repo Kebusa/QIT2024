b677a80022beb0a0595e12e7f933fa2fa12e26f2
Asubek <asubekerakyluly@gmail.com>
2024-11-04 14:40:06 +0500

    beta home work

---
+++ QIT2024/Examples/04.11/script.js
@@ -0,0 +1,113 @@
+// var a = 20 
+// var b = 100
+
+// console.log( a == b) //ten ekenin tekseru
+// console.log( a > b)
+// console.log( a < b)
+// console.log( a != b) //ten emes
+// console.log(a >= b) // ulken nemese ten
+// console.log(a <= b) //kishi nnemese ten
+
+// console.log("10 men 100 din arasynda jatr ma:", a >= 10 && a <= 100) //jane
+
+// if(a >= 10 && a <= 100){
+//     console.log("diapazonda jatr")
+// }
+// else if(a > 100){
+//     console.log("a 100 den artyk")
+// }
+
+// var a = 5 
+// var b = 3
+// var c = 4 
+
+// console.log((a > b) && (c < a)) //jane
+// console.log((a < b) || (c < a)) //nemese (bir shart oryndalgany jetkilikti)
+
+// var isOffrRoad = false
+
+// console.log(!isOffrRoad) // "!" teris koldanylady  !false = true
+// console.log (3 != 4)
+
+
+// let age = 20 
+// let hasTicket = true
+// let isVIP = false
+
+// if((age >= 18 && hasTicket) || isVIP){
+//     console.log("bolady")
+// }
+// else{
+//     console.log("bolmaidy")
+// }
+
+// var fuel = "gas"
+// var color = 'black'
+// var engine = 2.5
+// var mark = "Nissan"
+
+// if(mark == "Nissan" && fuel == "gas"){
+//     console.log("beru")
+// }
+// else if(engine >= 2.5 || color =='black'){
+//     console.log("ok")
+// }
+// else{
+//     console.log("jaramaidy")
+// }
+
+
+// var age = 30 
+
+// console.log(12 < age && 45 > age)
+
+// var n = 21
+
+// console.log((n % 2 == 0)&&(n <= 20 && n >= 50 ))
+
+
+// var a = 8
+// var b = 8
+// var per = 2 *(a + b)
+
+// console.log((a == b) && (30 <= per && 40 >= per))
+
+
+// //task1
+
+// var a = 50
+// var b = 40
+
+// console.log((a % 3 == 0)||(b % 5 == 0))
+
+// //task2
+
+// var temp1 = 17
+// var temp2 = 40
+
+// console.log((15 <= temp1 && 25 >= temp1) && (temp2 >= 30))
+
+// //task3
+
+// var m = 100
+// var s = 30 
+// total = m - s 
+// console.log((50 >= total) && (s <= 20))
+
+// //task4
+// var score1 = 50
+// var score2 = 70
+// var score3 = 10
+// avarageScore = (score1 + score2 + score3) / 3
+
+// var A = (70 <= avarageScore && 90 >= avarageScore)
+// var B = (score1 >= 75 || score2 >=75 || score3 >=75)
+// var C = (score1+score2 < score3)
+
+// if(A && B && C == true){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
---
+++ QIT2024/homeworks/04.11/script.js
@@ -0,0 +1,43 @@
+// task1
+var age = 19;
+
+console.log(age >= 18);
+
+
+// task2
+var num = 20
+
+if(num % 2 == 0){
+    console.log("Жұп")
+}
+else{
+    console.log("Тақ")
+}
+
+
+//task3
+
+
+
+
+//task4
+var a = 50
+
+
+if(a > 0){
+    console.log("Оң сан")
+}
+else{
+    console.log("Теріс сан")
+}
+
+//task5 
+var f = 20
+var t = 50
+
+if((f % 5 == 0) && (t % 10 == 0)){
+    console.log("Шын")
+}
+else{
+    console.log("Жалғын")
+}
