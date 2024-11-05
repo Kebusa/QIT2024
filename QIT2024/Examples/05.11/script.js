1fe6faf1361df4b4752c95b2dd573177d533fe6c
Asubek <asubekerakyluly@gmail.com>
2024-11-05 17:30:46 +0500

    home work 05.11

---
+++ QIT2024/Examples/05.11/script.js
@@ -0,0 +1,112 @@
+// alert("hello world! "+ "Hello " + "\n" + "2024!")
+
+// prompt("how are you feeling:")
+
+// let name = prompt("enter your name:")
+// let age = prompt("Enter your age:")
+// let email = prompt("enter youre email:")
+
+
+// alert("Hello " + name + "\n" +"you are " + age + " y.o." +"\n" +"youre email: " + email)
+
+
+
+
+// var num = prompt("enter num:")
+
+// if(num % 2 == 0){
+//     alert("Number " + num + " Jup")
+// }
+// else{
+//     alert("Number " + num + " Tak")
+// }
+
+
+
+// let birth = prompt("Enter youre birth:")
+
+// alert("youre birthday: " + birth + "\n" + " jas:" + (2024 - birth))
+
+
+
+
+// var birth =  Number(prompt("Enter youre number:"))
+// var age = "Youre age: " + (2024 - birth)
+
+// if(isNaN(birth)){
+//     alert("Enter number!")
+// }
+// else{
+//     alert(age)
+// }
+
+
+
+
+// var password = prompt("Enter youre pass:")
+// var correctPass = "1234"
+
+// if(password == correctPass){
+//     alert("Welcome Boss!!")
+// }
+// else{
+//     alert("Uncorect pass! " + password)
+// }
+
+
+
+
+// var ball = 0
+
+
+
+// var question1 = Number(prompt("75 * 65"))
+// if(question1 == (75 * 65)){
+//     ball++
+//     alert(question1 + " correct")
+//     }
+//     else if(question1 != (75 * 65)){
+//         alert(question1 + " no correct!")
+//     }
+// var question2 = Number(prompt("50 / 20"))
+
+// if(question2 == (50 / 20)){
+//     ball++
+//     alert(question2 + " correct")
+// }
+// else if(question2 != (50 / 20)){
+//     alert(question2 + " no correct!")
+// }
+// var question3 = Number(prompt(" 40 - 50"))
+// if(question3 == (40 - 50)){
+//     ball++
+//     alert(question3 + " correct")
+// }
+// else if(question3 != (40 - 50)){
+//     alert(question3 + " no correct!")
+// }
+// var question4 = Number(prompt("30 + 15"))
+
+// if(question4 == (30 + 15)){
+//     ball++
+//     alert(question4 + " correct")
+// }
+// else if(question4 != (30 + 15)){
+//     alert(question4 + " no correct!")
+// }
+// var question5 = Number(prompt("88 * 88"))
+// if(question5 == (88 * 88)){
+//     ball++
+//     alert(question5 + " correct")
+// }
+// else if(question5 != (88 * 88)){
+//     alert(question5 + " no correct!")
+// }
+
+
+
+// alert("youre score: " + ball)
+
+
+
+
