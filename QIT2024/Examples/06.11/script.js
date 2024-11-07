6661d74fa6623cf756cf461e98a344760fc90895
Asubek <asubekerakyluly@gmail.com>
2024-11-07 11:04:49 +0500

    exercise

---
+++ QIT2024/Examples/06.11/script.js
@@ -0,0 +1,113 @@
+
+// var isRain = prompt("kazir janbyr ma? ")
+
+// if(isRain == "Ya" || isRain == "ya"){
+//     alert("avtobuspen bar!")
+// }
+// else if(isRain == "jok"){
+//     var isMorning = prompt("Kazir tan ba?")
+
+//     if(isMorning  == "Ya" || isMorning == "ya"){
+//         alert("velosipedpen bar!")
+//     }
+//     else if (isMorning == "jok"){
+        
+//         var car = prompt("kolik bar ma?")
+//         if (car == "bar" || car =="ya"){
+//             alert("machinamen bar!")  
+//         }
+//         else if (car == "jok"){
+//             alert("jayau bar")
+//         }
+//     }
+//     else{
+//         alert("tek ya nemese jok dep jauap ber! ")
+//     }
+// }
+// else{
+//     alert("tek ya nemese jok dep jauap ber! ")
+// }
+
+// var age = prompt("Жасың")
+
+// if(age <= 17){
+//     alert("Кішкентайсың!")
+// }
+// else if(age >=18){
+//   var pol = prompt("Male")
+//   alert ("A) girl" + "\n" + "B) Boy" + "\n" + "C) Other")
+//   if(pol == "A" || pol == "a"){
+//     var isRain = prompt("Жаңбыр ма?")
+//     if(isRain == "Иә" || isRain == "иә" ){
+//         alert("Қалың киін")
+//     }
+//     else if(isRain == "" || isRain == ""){
+//         alert("полюбому қалың киін")
+//     }
+//   }
+//   else if (pol == "B" || pol == "b"){
+//     var isSunny  = prompt("")
+//     if(isSunny == "" || isSunny == ""){
+//         alert("")
+//     }
+//     else if (isSunny == "" || isSunny == ""){
+//         alert("")
+//     }
+//   }
+//   else if (pol == "C" || pol == "c"){
+//     alert("go away and no return!!!")
+//   }
+
+
+// }
+// else{
+//     alert("Сан ғана жаз!")
+// }
+
+
+// var name = prompt("Youre name: ")
+
+// var one = prompt("Коффеге заказ беріңіз:" + "\n" + "A) Capuchino" + "\n" + "B) Americano" + "\n" + "C) Latte")
+// if(one == "A" || one == "a" || one == "c" || one == "C"){
+//     var milk = prompt("Қандай сүт:" + "\n" + "A) Авсеное сүт" + "\n" + "B) Қолдың сүті" + "\n" + "C) Дүкен сүті")
+//     if(milk == "a" || milk == "A"){
+//         var dam = prompt("Қандай дәм қалайсыз:" +"\n"+"A) coconut"+"\n" +"B) banan" + "\n" + "C) comon")
+//         if(dam == "c" || dam == "C"){
+//             alert("ол сүт бітті!")
+//         }
+//         else {
+//             var dobavka = prompt("Сироп Қосасыз ба?" + " \n" + " Иә"+ "\n" + "Жоқ")
+//             if(dobavka == "Иә" || dobavka == "иә"){
+//                 var sirop = prompt("Қандай сироп таңдайсың:" + "\n" + "A) Chocolate" + "\n" + "B) Solt caramel" +  "\n" + "C)Caramel")
+//             }
+//             else{
+        
+    
+//             }
+            
+//         }
+//     }
+//     else{
+//         var dobavka = prompt("Сироп Қосасыз ба?" + " \n" + " Иә"+ "\n" + "Жоқ")
+//         if(dobavka == "Иә" || dobavka == "иә"){
+//             var sirop = prompt("Қандай сироп таңдайсың:" + "\n" + "A) Chocolate" + "\n" + "B) Solt caramel" +"\n" + "C)Caramel")
+//         }
+//         else{
+    
+
+//         }
+//     }
+// }
+
+// else if(one == "b " || one == "B"){
+//     var dobavka = prompt("Сироп Қосасыз ба?" + " \n" + " Иә"+ "\n" + "Жоқ")
+//     if(dobavka == "Иә" || dobavka == "иә"){
+//         var sirop = prompt("Қандай сироп таңдайсың:" + "\n" + "A) Chocolate" + "\n" + "B) Solt caramel" + "\n" + "C)Caramel")
+//     }
+//     else{
+        
+
+//     }
+
+// }
+// alert(one + "\n" + milk + "\n" + dam + "\n" + dobavka + "\n" + sirop +"\n"  + name)
