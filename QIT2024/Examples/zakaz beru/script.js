9a005378ce905467b474c37dfe21c2962044afa5
Asubek <asubekerakyluly@gmail.com>
2024-11-07 12:43:18 +0500

    Заказ беру жане Чек

--- QIT2024/Examples/zakaz beru/script.js
+++ QIT2024/Examples/zakaz beru/script.js
@@ -1,35 +1,64 @@
-
-var coffe = prompt("Қандай коффе Қалайсыз: " + " \n" + "A) Americano" +"\n" + "B) Capuchino" + " \n" + "C)Latte" )
+var name = prompt("Өз атыңды жаз: ")
+var coffe = prompt("Қандай коффе Қалайсыз: " + " \n" + "A) Americano 5€" +"\n" + "B) Capuchino 6€" + " \n" + "C)Latte 7€" )
 var chek = ""
-
+var sum = 0;
 
 switch(coffe){
-    case "A","a":
-        chek += "Americano"
+    case "A" :
+        chek += "Americano 5€ \n"
+        sum += 5
     break
-    case "B","b":
-        chek += "Capuchino"
+    case "B" :
+        chek += "Capuchino 6€ \n"
+        sum += 6
     break
-    case "C","c":
-        chek += "Latte" 
+    case "C":
+        chek += "Latte 7€ \n" 
+        sum += 7
     break
     default:
         alert("Тек вариант жазыңыз!")
 }
-var milk = prompt("Сүт Таңдаңыз: " + "\n" + "A) Пакеттегі сүт" + "\n" + "B) Авсяное " + "\n" + "C) Қол сүті" )
+var milk = prompt("Сүт Таңдаңыз: " + "\n" + "A) Пакеттегі сүт 0.5€" + "\n" + "B) Авсяное 0.9€ " + "\n" + "C) Қол сүті 0.2€" )
 
 switch(milk){
-    case "a","A":
-        chek += "Пакеттегі сүт"
+    case "A" :
+        chek += "Пакеттегі сүт 0.5€ \n"
+        sum += 0.5
     break
-    case "b","B":
-        chek += "Авсяное"
+    case "B" :
+        chek += "Авсяное 0.9€ \n"
+        sum += 0.9
     break
-    case "c","C":
-        chek += "Қол сүті"
+    case "C" :
+        chek += "Қол сүті 0.2€ \n"
+        sum += 0.2
     break
     default:
         alert("Тек вариант жазыңыз!")
         
 }
-alert(chek)
+var sirop = prompt("Сироп Таңдаңыз: " + "\n" + "A) Шокалад 1.5€" + "\n" + "B) Карамель 1.5€ " + "\n" + "C) Соленая карамель 1.5€" )
+
+switch(milk){
+    case "A" :
+        chek += "Шокалад 1.5€ \n"
+        sum += 1.5
+    break
+    case "B" :
+        chek += "Карамель 1.5€ \n"
+        sum += 1.5
+    break
+    case "C" :
+        chek += "Соленая карамель 1.5€ \n"
+        sum += 1.5
+    break
+    default:
+        alert("Тек вариант жазыңыз!")
+        
+}
+
+
+
+
+alert(chek + "\n" + "Бағасы: "+ sum + "€" + "\n" + "Клиент аты-жөні:  " + name)
