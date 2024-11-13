3532ac1285594ee98a430b43c682200ad8ff7580
Asubek <asubekerakyluly@gmail.com>
2024-11-13 22:54:20 +0500

    13.11 home work

--- QIT2024/homeworks/13.11/script.js
+++ QIT2024/homeworks/13.11/script.js
@@ -1,3 +1,25 @@
+
+// task 1
+let num = 0
+do{
+    num++
+    if(num % 3 == 0 && num % 5 == 0){
+        console.log("FizzBuzz")
+    }
+    else if (num % 3 == 0){
+        console.log("Fizz")
+    }
+    else if (num % 5 ==0){
+        console.log("Buzz")
+    } else if (num % 2 != 0) {
+        console.log(num);
+        
+    }
+}while(num < 40)
+
+
+
+// task 2
 let num = 20
 let quest = ""
 let quest1 = ""
@@ -35,3 +57,28 @@ while(true){
         }else{
         num--
 }}
+
+
+//task3 
+
+var three = 1
+var seven = 1
+var i = prompt("Диапазонын басталу сан енгізіңіз:")
+var end = prompt("Диапазонын аяақтайтын сан енгізіңіз:")
+
+
+do{
+    if(i % 3 == 0){ 
+        three *= i
+        console.log(three)
+    } 
+    if(i % 7 == 0){
+        seven *= i
+        console.log(seven)
+    }
+    i++
+
+}while(i <= end)
+    console.log(three)
+    console.log(seven)
+
