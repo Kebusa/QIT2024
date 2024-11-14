8f1033e60ae7ce14f1b3958190ab3f1356fc9bbb
Asubek <asubekerakyluly@gmail.com>
2024-11-14 12:54:19 +0500

    14.11 example

---
+++ QIT2024/Examples/14.11/script.js
@@ -0,0 +1,79 @@
+// var student = +prompt("Student sany")
+// var graduent = ""
+// var total = 0
+// var num = 1
+
+
+// do{
+//     graduent = +prompt("baga jaz")
+//     if(graduent <= 0 || graduent >= 100){
+//         graduent = +prompt("0 den 100 ge deiyn jaz " )
+//     }
+//     total = graduent + total
+//     num++
+   
+// }while(num <= student)
+//     console.log(total / student)
+
+
+// var bir = +prompt("birinshi san")
+// var eki = +prompt("ekinshi san")
+// var total = 0
+// var total2 = 0
+// var minNum;
+// var maxNum;
+
+
+
+// if(bir > eki)
+// {
+//     minNum = eki
+//     maxNum = bir
+// }
+// else{
+//     minNum = bir
+//     maxNum = eki
+// }
+
+
+// for (let i = minNum; i <= maxNum; i++){
+//     if(i % 2 == 0){
+//         console.log(i +" jup sandar")
+//         total += i
+//     }
+//     else{
+//         console.log(i + " tak sandar")
+//         total2 +=i
+//     } 
+    
+// }
+// console.log(`jup sandar kosyndysy ${total} bolady`)
+// console.log(`tak sandar kosyndysy ${total2} bolady`)
+
+
+
+// var num1 = 1 
+// var num2 = 30
+// for(let i = num1; i <= 30; i++){
+//     if(i % 3 == 0){
+//         console.log(i)
+//     }
+// } 
+
+
+// var num1 = 50 
+// var num2 = 0
+// for(let i = num1; i >= num2; i-- ){
+//     if(i % 5 == 0){
+//         console.log(i)
+//     }
+// }
+
+// for (let i = 1; i <= 15; i++){
+//     if(i % 4 == 0){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
