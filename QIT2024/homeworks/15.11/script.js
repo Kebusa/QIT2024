5520006c1871d7bfc3fbc840730dbd5173f6e4ff
Asubek <asubekerakyluly@gmail.com>
2024-11-16 11:08:20 +0500

    15.11 home work

---
+++ QIT2024/homeworks/15.11/script.js
@@ -0,0 +1,12 @@
+var pass = prompt("durys parol jaz")
+
+for (let i = 1; i <= 5; i++){
+    if(pass == "amjilt_the_best"){
+        alert("welcome")
+        break;
+    }
+    else{
+        prompt("too many attempts")
+    }
+    
+}
