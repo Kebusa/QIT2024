bcfbb6ce1b6801d6c4d2da4df2c00783f646f81d
Asubek <asubekerakyluly@gmail.com>
2024-11-20 13:03:40 +0500

    example

---
+++ QIT2024/Examples/20.11/script.js
@@ -0,0 +1,125 @@
+// let num1 = +prompt("San engiz:")
+// let sign = +prompt("Tanba engiz:")
+// let num2 = +prompt("San engiz:")
+
+// function calc(a,b,x){
+//     switch(x){
+//         case "+":
+//             return a + b
+//         case "-":
+//             return a - b
+//         case "*":
+//             return a * b
+//         case "/":
+//             return a / b
+//     }
+// }
+
+// alert(calc(num1, num2 , sign))
+
+
+
+// salystyru
+// let num1 = +prompt("San engiz:")
+// let num2 = +prompt("San engiz:")
+// let num3 = +prompt("San engiz:")
+
+// function findGreatest(a, b, c){
+//     if(a > b && a > c){
+//         return a
+//     }else if(b > a && b > c){
+//         return b
+//     }else if(c > a && c > b){
+//         return c
+//     }
+// }
+
+
+// var a = +prompt("Cm")
+// const smToKm = (number) => {
+//     return number / 100000 + " km"
+// }
+// alert( smToKm(a) );
+
+
+
+
+// let num1 = +prompt("San jaz:")
+// let currence = prompt("dollar nemese tenge:")
+
+// const auystyru = (a, b) =>{
+//     switch(b){
+//         case "dollar":
+//             return a * 495.35  + " ₸"
+//         case "tenge":
+//             return a * 0.002019 + " $"
+//         default:
+//             alert("durys jazylmady!")
+
+//     }
+// }
+//     alert(auystyru(num1, currence))
+
+
+
+// function shygyn(){
+//     let result = 0;
+//     let q;
+
+//     do{
+//         q = prompt("Tauar")
+//         if(q != "Аяақтау"){
+//             let baga = +prompt("Tauar sany")
+//             result = result + baga
+
+//         }
+
+//     }while(q != "Аяақтау")
+//         return result
+// }
+
+//     alert(shygyn())
+
+
+
+// const kosu = (a, b) =>{
+//     return a + b;
+// }
+// console.log(kosu(4, 6))
+
+
+// const findLength = (text) => {
+//     return text.length;
+// }
+// console.log(findLength("Hello world"))
+
+
+// function valuta(){
+//     var result = 0
+//     var sum ;
+//     var val;
+//     do{
+       
+//         val = prompt("eur, tg nemese usd")
+//         if(val == "eur"){
+//             sum = +prompt("Baga jazynyz")
+//             result = sum * 1.0588 + " USD" + "\n" + sum / 1 +" EUR" + "\n" + sum * 524.11 + " TG"
+//         }
+//         else if(val == "usd"){
+//             sum = +prompt("Baga jazynyz")
+//             result = sum * 1 + " USD" + "\n" + sum * 0.944465 +" EUR" + "\n" + sum * 495.35 + " TG" 
+//         }
+//         else if(val == "tg"){
+//             sum = +prompt("Baga jazynyz")
+//              result = sum * 0.002019 + " USD" + "\n" + sum * 0.001908 +" EUR" + "\n" + sum * 1 + " TG"
+//         }
+
+
+//     }while(val != "0")
+//         return result
+// }
+
+// console.log(valuta())
+
+
+
