da5b36ebf719f71ab248f41dae1d0fa989146565
Asubek <asubekerakyluly@gmail.com>
2024-11-27 12:59:51 +0500

    27.11 example

---
+++ QIT2024/Examples/27.11/script.js
@@ -0,0 +1,135 @@
+// let citis = ["Almaty"," Taldykorgan", "Aktau", "Atyrau", "Astana"]
+
+// citis.push("NewYourk", "Canada")
+// citis.unshift("Africa")
+// citis.pop()
+// citis.shift()
+// console.log(citis)
+// console.log(citis[2])
+
+// citis[3] = "Kokchetau"
+// console.log(citis)
+// citis[citis.length - 1] = "Oral"
+// console.log(citis);
+
+
+
+// let a = 5
+// let b = a
+// b = 10
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+// referense type ссылочный тип
+// let arr1 = [a, 2, 3, 4, 5]
+// let arr2 = arr1
+
+// arr2[0] = 0
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+
+// let numbers = [5,2,8,1,9,7]
+// numbers.sort((a, b) =>  b - a) // кему ретімен
+// console.log(numbers);
+
+
+// numbers.sort((a, b) => a - b) // өсу ретімен
+// console.log(numbers);
+
+// numbers.reverse()
+// console.log(numbers);
+
+// numbers.slice();//[5,2,8,1,9,7]
+// numbers.slice(2)//[8,1,9,7]
+// let sliceArr = numbers.slice(2,4)//[8, 1]
+
+// console.log(numbers);
+
+// console.log(sliceArr);
+
+// numbers.splice(2, 2); // индекстан кейін элемент алып тастайды
+// console.log(numbers);
+
+
+// numbers.splice(2, 2, 14, 17, 18); // келесі сандарды орнына қосады
+// console.log(numbers);
+
+
+// numbers.includes(7) // true // масивтің ішінен іздейді
+
+// if(numbers.includes(9)){
+//     console.log("found 9 in the array");
+    
+// }else{
+//     console.log("not in the array");
+    
+// }
+
+
+// let inp = +prompt("Enter number:")
+
+// if(numbers.includes(inp)){
+//     console.log("found 9 in the array");
+    
+// }else{
+//     console.log("not in the array");
+    
+// }
+
+
+// let ind = numbers.indexOf(7) // Индексін қайтарады егер folse -1
+
+
+// let citi = ["Almaty"," Taldykorgan", "Aktau", "Atyrau", "Astana"]
+
+// let find = prompt("Enter siti")
+
+// if(citi.indexOf(find) >= 0){
+//     console.log(find + " we have");
+    
+// }else{
+//     console.log(find + " we have not");
+    
+// }
+
+
+
+
+// let citi = ["Almaty","Taldykorgan", "Aktau", "Atyrau", "Astana"]
+
+// let cities = citi.join("-") // масивті стрингке айландырады
+
+// console.log(cities);
+
+
+// let worldCities = ["New York", "London", "Paris"]
+
+// let cities = citi.concat(worldCities, citi)
+
+// console.log(cities);
+
+
+// let citi = "Almaty, Taldyk, Shymkent"
+
+// let bolu = citi.split(", ") //пробелымен қосып бөледі
+
+// console.log(bolu);
+
+
+let kala = prompt("kai kalada boldynyz")
+
+let mass = kala.split(", ")
+
+console.log(mass.length + " Kalada boldynyz");
+
+
+
+
+
