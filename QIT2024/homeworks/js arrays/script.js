6624f52370a04b1d3937566744303dae070a81b5
Asubek <asubekerakyluly@gmail.com>
2024-11-29 13:07:06 +0500

    home work 28.11

---
+++ QIT2024/homeworks/js arrays/script.js
@@ -0,0 +1,16 @@
+// task 1 
+
+let numbers = [2, 4, 6, 8];
+let add = numbers.map(kob => kob * 3)
+
+console.log(add);
+
+
+
+// task 2 
+
+let number = [3, 7, 1, 9, 4]
+let five = number.filter(n => n > 5)
+
+console.log(five);
+
