d3b95be29e01868dd6d588e8a9ff069598ff424b
Asubek <asubekerakyluly@gmail.com>
2025-01-20 13:30:02 +0500

    home work 14.01.25

--- QIT2024/homeworks/14.01.25/script.js
+++ QIT2024/homeworks/14.01.25/script.js
@@ -1,18 +1,25 @@
 const api = "https://jsonplaceholder.typicode.com/users"
 const container = document.getElementById('container')
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const count = 0
+
 
 function fetchData() {
     fetch(api)
     .then(response => response.json())
     .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            container.innerHTML = `
-            <p>Қолданушы аты: ${data[i].name}</p>
-            <p>Email: ${data[i].email}</p>` 
-        }
+     data.forEach(user => {
+        const userName = user.name
+        const userEmail = user.email
+        const className = document.createElement('p')
+        const classEmail = document.createElement('p')
+        const allClass = document.createElement('div')
+
+        className.textContent = userName
+        classEmail.textContent = userEmail
+
+        allClass.appendChild(className)
+        allClass.appendChild(classEmail)
+        container.appendChild(allClass)
+     });
        
     })
     .catch(error => "Қате орын алды!" + error)
